3f3d7b447d39888b5880593da69588678bce0b4a
Ruslan Moskvitin <moskvitin.ra@phystech.edu>
2021-12-23 17:13:48 +0300

    bugfix: переименовал БД с diplomadb на bitfilmsdb

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/diplomadb', {
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
 
